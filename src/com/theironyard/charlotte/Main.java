ef731aaf5911513d99751426bce4f0e058e4d077
Emileen Marianayagam <emileenmarianayagam@Emileens-MacBook-Pro.local>
2016-12-13 15:38:46 -0500

    inital attempt

--- src/com/theironyard/charlotte/Main.java
+++ src/com/theironyard/charlotte/Main.java
@@ -3,6 +3,56 @@ package com.theironyard.charlotte;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        // write your code here
+        /*for (int i = 0; i < 1; i++) {
+            System.out.println(fizzBuzz(i));
+        }
+
+        //*/
+
+        fizzBuzz();
+    }
+
+    public static void fizzBuzz() {
+        //If the value is evenly divisible by 3, print "Fizz"
+        //If the value is evenly divisible by 5, print "Buzz"
+        //If the value is evenly divisible by both 3 and 5, print "fizzBuzz"
+        // Otherwise, print the number
+        for (int i = 1; i <= 100; i++) {
+
+            if (i % 3 == 0 && i % 5 == 0) {
+                System.out.println("FIZZBUZZ");
+            } else if (i % 5 == 0) {
+                System.out.println("BUZZ");
+            } else if (i % 3 == 0) {
+                System.out.println("FIZZ");
+            } else {
+                System.out.println(i);
+            }
+
+        }
+
+
+    /*public static String fizzBuzz(int i) {
+        //If the value is evenly divisible by 3, print "Fizz"
+        //If the value is evenly divisible by 5, print "Buzz"
+        //If the value is evenly divisible by both 3 and 5, print "fizzBuzz"
+        // Otherwise, print the number
+        for (i = 1; i <= 100; i++) {
+
+            if (i % 3 == 0 && i % 5 == 0) {
+                System.out.println("FIZZBUZZ");
+            } else if (i % 5 == 0) {
+                System.out.println("BUZZ");
+            } else if (i % 3 == 0) {
+                System.out.println("FIZZ");
+            } else {
+                System.out.println(i);
+            }
+
+       }
+        return fizzBuzz(i);
+
+    }*/
     }
 }

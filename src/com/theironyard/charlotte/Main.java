a8bb4d21ecc399b8be9388b8c0c421acd630c217
Emileen Marianayagam <emileenmarianayagam@Emileens-MacBook-Pro.local>
2016-12-14 06:05:09 -0500

    making progress

--- src/com/theironyard/charlotte/Main.java
+++ src/com/theironyard/charlotte/Main.java
@@ -1,8 +1,14 @@
 package com.theironyard.charlotte;
 
+import com.sun.tools.javac.util.ArrayUtils;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
 public class Main {
 
     public static void main(String[] args) {
+
         // write your code here
         /*for (int i = 0; i < 1; i++) {
             System.out.println(fizzBuzz(i));
@@ -10,7 +16,12 @@ public class Main {
 
         //*/
 
-        fizzBuzz();
+        //fizzBuzz();
+        //fibonacciSequence(10);
+        //System.out.println("----------------------");
+        boost(4256);
+
+
     }
 
     public static void fizzBuzz() {
@@ -31,6 +42,7 @@ public class Main {
             }
 
         }
+    }
 
 
     /*public static String fizzBuzz(int i) {
@@ -54,5 +66,63 @@ public class Main {
         return fizzBuzz(i);
 
     }*/
+
+    public static int[] fibonacciSequence(int x) {
+
+        // given a specific number print out the sequence for the fibonacci
+        //fib(n) = fib(n-1) + fib(n-2)
+
+        int[] fib = new int[x];
+        fib[0] = 0;
+        fib[1] = 1;
+
+        for (int a = 2; a < x; a++) {
+            fib[a] = fib[a - 1] + fib[a - 2];
+            System.out.print(" " + fib[a]);
+
+
+        }
+
+
+        return fib;
+
     }
+
+    public static int[] boost(int a) {
+
+        // take the number from the user
+        //use the integer.toString to convert the into to string
+        // take the string and get the character at each point and store it into array
+        //convert the character into int
+        // check if the int is between 1-8 then add 1 or if its 9 change to 0
+        //put the indiviudal int together and print it out
+
+        String passedA = Integer.toString(a);
+        System.out.println(passedA);
+        int[] number = new int[passedA.length()];
+        //System.out.println(number[1]);
+
+        for (int i = 0; i < passedA.length() ; i++) {
+
+            number[i] = a % 10;
+            a = a / 10;
+
+
+            if (number[i] == 9) {
+                number[i] = 0;
+            } else {
+                number[i] += 1;
+            }
+
+            System.out.print(number[i]);
+
+        }
+
+
+        return number;
+    }
+
+
 }
+
+

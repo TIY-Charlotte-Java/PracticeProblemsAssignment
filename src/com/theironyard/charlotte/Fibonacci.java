c002cd07823956379a164c5e163745d03ed6caa9
Ben <bburrva@gmail.com>
2016-12-15 11:49:16 -0500

    revision

--- src/com/theironyard/charlotte/Fibonacci.java
+++ src/com/theironyard/charlotte/Fibonacci.java
@@ -5,13 +5,12 @@ package com.theironyard.charlotte;
  */
 public class Fibonacci {
     double fibbonaci(int n){
-        Double fin;
         double prev=0d, next=1d, result=0d;
         for (int i = 0; i < n; i++) {
             result=prev+next;
             prev=next;
             next=result;
-            //System.out.println("The result is: " + result);
+            System.out.println("The result is: " + result);
         }
         System.out.println("The result is: " + result);
         return result;
--- test/com/theironyard/charlotte/MainTest.java
+++ test/com/theironyard/charlotte/MainTest.java
@@ -1,8 +1,18 @@
 package com.theironyard.charlotte;
 
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
 /**
  * Created by Ben on 12/13/16.
  */
 public class MainTest {
 
+    @Test
+    public void boostWorks() throws Exception {
+        assertEquals(53, Increment.boost(42));
+        assertEquals(2990, Increment.boost(3001));
+        assertEquals(0, Increment.boost(999999));
+    }
 }
